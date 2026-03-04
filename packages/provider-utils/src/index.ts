fa1685d04048e3288f42941209c532212603ecf5
Kristen T. Tran <kristenloveboba.@gmail.com>
2026-03-04 11:00:21 +0700

    Initialize patch-4

---
+++ packages/provider-utils/src/index.ts
@@ -0,0 +1,58 @@
+export * from './combine-headers';
+export { convertAsyncIteratorToReadableStream } from './convert-async-iterator-to-readable-stream';
+export * from './delay';
+export * from './extract-response-headers';
+export * from './fetch-function';
+export { createIdGenerator, generateId, type IdGenerator } from './generate-id';
+export * from './get-error-message';
+export * from './get-from-api';
+export { getRuntimeEnvironmentUserAgent } from './get-runtime-environment-user-agent';
+export { injectJsonInstructionIntoMessages } from './inject-json-instruction';
+export * from './is-abort-error';
+export { isNonNullable } from './is-non-nullable';
+export { isUrlSupported } from './is-url-supported';
+export * from './load-api-key';
+export { loadOptionalSetting } from './load-optional-setting';
+export { loadSetting } from './load-setting';
+export { type MaybePromiseLike } from './maybe-promise-like';
+export { mediaTypeToExtension } from './media-type-to-extension';
+export { normalizeHeaders } from './normalize-headers';
+export * from './parse-json';
+export { parseJsonEventStream } from './parse-json-event-stream';
+export { parseProviderOptions } from './parse-provider-options';
+export * from './post-to-api';
+export {
+  createProviderDefinedToolFactory,
+  createProviderDefinedToolFactoryWithOutputSchema,
+  type ProviderDefinedToolFactory,
+  type ProviderDefinedToolFactoryWithOutputSchema,
+} from './provider-defined-tool-factory';
+export * from './remove-undefined-entries';
+export * from './resolve';
+export * from './response-handler';
+export {
+  asSchema,
+  jsonSchema,
+  lazySchema,
+  zodSchema,
+  type FlexibleSchema,
+  type InferSchema,
+  type LazySchema,
+  type Schema,
+  type ValidationResult,
+} from './schema';
+export * from './uint8-utils';
+export * from './validate-types';
+export { VERSION } from './version';
+export { withUserAgentSuffix } from './with-user-agent-suffix';
+export * from './without-trailing-slash';
+
+// folder re-exports
+export * from './types';
+
+// external re-exports
+export * from '@standard-schema/spec';
+export {
+  EventSourceParserStream,
+  type EventSourceMessage,
+} from 'eventsource-parser/stream';

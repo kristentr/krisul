39443a845be4dad6974485f4cd8ac2ee220a3da5
Kristen T. Tran <kristenloveboba.@gmail.com>
2026-03-04 10:59:52 +0700

    Initial commit

---
+++ packages/ai/src/model/as-embedding-model-v3.ts
@@ -0,0 +1,18 @@
+import { EmbeddingModelV2, EmbeddingModelV3 } from '@ai-sdk/provider';
+
+export function asEmbeddingModelV3(
+  model: EmbeddingModelV2<string> | EmbeddingModelV3,
+): EmbeddingModelV3 {
+  if (model.specificationVersion === 'v3') {
+    return model;
+  }
+
+  // TODO this could break, we need to properly map v2 to v3
+  // and support all relevant v3 properties:
+  return new Proxy(model, {
+    get(target, prop: keyof EmbeddingModelV2<string>) {
+      if (prop === 'specificationVersion') return 'v3';
+      return target[prop];
+    },
+  }) as unknown as EmbeddingModelV3;
+}
